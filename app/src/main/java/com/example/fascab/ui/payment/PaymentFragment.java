b23822f52bd403bf9dd99010d8b9154057fb845b
AhmadRazaJamal <ahmad.raza@me.com>
2020-03-30 19:16:24 -0700

    Uploaded payment and trips page

--- app/src/main/java/com/example/fascab/ui/payment/PaymentFragment.java
+++ app/src/main/java/com/example/fascab/ui/payment/PaymentFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,14 +25,18 @@ public class PaymentFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         paymentViewModel =
                 ViewModelProviders.of(this).get(PaymentViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_payment, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        paymentViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        Button b1 = getActivity().findViewById(R.id.rideNow);
+        b1.setVisibility(View.GONE);
+        Button b2 = getActivity().findViewById(R.id.later);
+        b2.setVisibility(View.GONE);
+
     }
 }
--- app/src/main/java/com/example/fascab/ui/trips/TripsFragment.java
+++ app/src/main/java/com/example/fascab/ui/trips/TripsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -23,13 +24,17 @@ public class TripsFragment extends Fragment {
         tripsViewModel =
                 ViewModelProviders.of(this).get(TripsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_trips, container, false);
-        final TextView textView = root.findViewById(R.id.text_tools);
-        tripsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        Button b1 = getActivity().findViewById(R.id.rideNow);
+        b1.setVisibility(View.GONE);
+        Button b2 = getActivity().findViewById(R.id.later);
+        b2.setVisibility(View.GONE);
+
+    }
+    
 }
